5d00f8870324d24593950a4b12059202c8bf9c8d
grandeurpawin <gpawin@byupathway.edu>
2026-05-19 17:56:11 +0800

    API test

---
+++ scripts/weather.js
@@ -0,0 +1,31 @@
+const currentTemp = document.querySelector("#current-temp");
+const weatherIcon = document.querySelector("#weather-icon");
+const captionDesc = document.querySelector("figcaption");
+
+const url = "https://api.openweathermap.org/data/2.5/weather?lat=49.77334055358263&lon=6.645329830973195&units=imperial&appid=8f43d38c43b9deadd688e86c587a83aa";
+
+async function apiFetch() {
+    try {
+        const response = await fetch(url);
+        if (response.ok) {
+            const data = await response.json();
+            console.log(data);
+            displayResults(data);
+        } else {
+            throw Error(await response.text());
+        }
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+function displayResults(data) {
+    currentTemp.innerHTML = `${data.main.temp}&deg;F`
+    const iconsrc = `https://openweathermap.org/img/w/${data.weather[0].icon}.png`;
+    let desc = data.weather[0].description;
+    weatherIcon.setAttribute("src", iconsrc);
+    weatherIcon.setAttribute("alt", desc);
+    captionDesc.textContent=`${desc}`
+}
+
+apiFetch();
